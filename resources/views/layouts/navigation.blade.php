30e2fbef9927fd446ab36d2d34af4ec845cffb54
NY-Jordan <yvanjordannguetse@yahoo.fr>
2022-08-03 13:51:02 +0200

    navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,92 +1,107 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+<nav class="main-sidebar ps-menu">
+    <div class="sidebar-toggle action-toggle">
+        <a href="#">
+            <i class="fas fa-bars"></i>
+        </a>
+    </div>
+    <div class="sidebar-opener action-toggle">
+        <a href="#">
+            <i class="ti-angle-right"></i>
+        </a>
+    </div>
+    <div class="sidebar-header">
+        <div class="text">AR</div>
+        <div class="close-sidebar action-toggle">
+            <i class="ti-close"></i>
         </div>
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
+    <div class="sidebar-content">
+        <ul>
+            <li class="active">
+                <a href="index.html" class="link">
+                    <i class="ti-home"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-desktop"></i>
+                    <span>UI Elements</span>
+                </a>
+                <ul class="sub-menu ">
+                    <li><a href="element-ui.html" class="link"><span>Elements</span></a></li>
+                    <li><a href="element-accordion.html" class="link"><span>Accordion</span></a></li>
+                    <li><a href="element-tabs-collapse.html" class="link"><span>Tabs & Collapse</span></a></li>
+                    <li><a href="element-card.html" class="link"><span>Card</span></a></li>
+                    <li><a href="element-button.html" class="link"><span>Buttons</span></a></li>
+                    <li><a href="element-alert.html" class="link"><span>Alert</span></a></li>
+                    <li><a href="element-themify-icons.html" class="link"><span>Themify Icons</span></a></li>
+                    <li><a href="element-modal.html" class="link"><span>Modal</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-book"></i>
+                    <span>Form</span>
+                </a>
+                <ul class="sub-menu ">
+                    <li><a href="form-element.html" class="link">
+                            <span>Form Element</span></a>
+                    </li>
+                    <li><a href="form-datepicker.html" class="link">
+                            <span>Datepicker</span></a>
+                    </li>
+                    <li><a href="form-select2.html" class="link">
+                            <span>Select2</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-notepad"></i>
+                    <span>Utilities</span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="error-404.html" target="_blank" class="link"><span>Error 404</span></a></li>
+                    <li><a href="error-403.html" target="_blank" class="link"><span>Error 403</span></a></li>
+                    <li><a href="error-500.html" target="_blank" class="link"><span>Error 500</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-layers-alt"></i>
+                    <span>Pages</span>
+                </a>
+                <ul class="sub-menu ">
+                    <li><a href="pages-blank.html" class="link"><span>Blank</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-hummer"></i>
+                    <span>Auth</span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="auth-login.html" target="_blank" class="link"><span>Login</span></a></li>
+                    <li><a href="auth-register.html" target="_blank" class="link"><span>Register</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="main-menu has-dropdown">
+                    <i class="ti-write"></i>
+                    <span>Tables</span>
+                </a>
+                <ul class="sub-menu ">
+                    <li><a href="table-basic.html" class="link"><span>Table Basic</span></a></li>
+                    <li><a href="table-datatables.html" class="link"><span>DataTables</span></a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="charts.html" class="link">
+                    <i class="ti-bar-chart"></i>
+                    <span>Charts</span>
+                </a>
+            </li>
+        </ul>
     </div>
-</nav>
+</nav> 
