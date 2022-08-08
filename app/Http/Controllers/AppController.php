dc6beb8a125047f1bbb2d55cc2a3184e6e0e419d
NY-Jordan <yvanjordannguetse@yahoo.fr>
2022-08-08 16:51:15 +0200

    corrections settings

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -45,11 +45,13 @@ class AppController extends Controller
     }
     public function updateLogo(Request $request)
     {
+        
         if (isset($request->logo) && !empty($request->logo)) {
+            $request->validate([
+                'logo' => 'required'
+            ]);
             $path = $request->file('logo')->storeAs(
-                'images',
-                $request->images,
-                'public'
+                'images', 'logo'.rand(1, 1000), 'public'
             );
             $settings = tbl_settings::find(1);
             $settings->logo = $path;
@@ -57,13 +59,65 @@ class AppController extends Controller
             return back()->with('message', 'operation successfully');
         }
         if (isset($request->favicon) && !empty($request->favicon)) {
-            $path = $request->file("logo")->storeAs(
-                'images',
-                $request->favicon,
-                'public'
+            $request->validate([
+                'favicon' => 'required'
+            ]);
+            $path = $request->file('favicon')->storeAs(
+                'images', 'fvcn'.rand(1, 1000), 'public'
             );
             $settings = tbl_settings::find(1);
             $settings->favicon = $path;
+            $settings->save();
+            return back()->with('message', 'operation successfully');
+        }
+        if (isset($request->top_bar)&&!empty($request->top_bar)) {
+            $request->validate([
+                'top_bar_email' => 'required',
+                'top_bar_phone' => 'required',
+            ]);
+            $settings = tbl_settings::find(1);
+            $settings->top_bar_email = $request->top_bar_email;
+            $settings->top_bar_phone = $request->top_bar_phone;
+            $settings->save();
+            return back()->with('message', 'operation successfully');
+
+
+        }
+        if (isset($request->general_section)&&!empty($request->general_section)) {
+            $request->validate([
+                'footer_col1_title' => 'required',
+                'footer_col2_title' => 'required',
+                'footer_col3_title' => 'required',
+                'footer_col4_title' => 'required',
+                'footer_copyright' => 'required',
+                'footer_email' => 'required',
+                'footer_phone' => 'required',
+                'footer_portfolio_item' => 'required',
+                'footer_recent_news_item' => 'required',
+
+            ]);
+            $settings = tbl_settings::find(1);
+            $settings->footer_col1_title = $request->footer_col1_title;
+            $settings->footer_col2_title = $request->footer_col2_title;
+            $settings->footer_col3_title = $request->footer_col3_title;
+            $settings->footer_col4_title = $request->footer_col4_title;
+            $settings->footer_copyright = $request->footer_copyright;
+            $settings->footer_email = $request->footer_email;
+            $settings->footer_phone = $request->footer_phone;
+            $settings->footer_recent_portfolio_item = $request->footer_portfolio_item;
+            $settings->footer_recent_news_item = $request->footer_recent_news_item;
+
+            $settings->save();
+            return back()->with('message', 'operation successfully');
+
+
+        }
+        if (isset($request->newsletter)&&!empty($request->newsletter)) {
+            $request->validate([
+                'newsletter_text' =>  'required'
+            ]);
+            $settings = tbl_settings::find(1);
+            $settings->newsletter_text = $request->newsletter_text;
             $settings->save();
             return back()->with('message', 'operation successfully');
         }
